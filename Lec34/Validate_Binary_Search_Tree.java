7bf6a438e7497ade51659751285b34378c2c31a0
monukumar98 <monu012as@gmail.com>
2024-03-10 19:31:21 +0530

    BST

--- Lec34/Validate_Binary_Search_Tree.java
+++ Lec34/Validate_Binary_Search_Tree.java
@@ -1,5 +1,49 @@
 package Lec34;
 
 public class Validate_Binary_Search_Tree {
+	public class TreeNode {
+		int val;
+		TreeNode left;
+		TreeNode right;
 
+		TreeNode() {
+		}
+
+		TreeNode(int val) {
+			this.val = val;
+		}
+
+		TreeNode(int val, TreeNode left, TreeNode right) {
+			this.val = val;
+			this.left = left;
+			this.right = right;
+		}
+	}
+
+	class Solution {
+		public boolean isValidBST(TreeNode root) {
+			return IsBst(root).bst;
+		}
+
+		public BstPair IsBst(TreeNode root) {
+			if (root == null) {
+				return new BstPair();
+			}
+			BstPair lbstp = IsBst(root.left);
+			BstPair rbstp = IsBst(root.right);
+			BstPair sbstp = new BstPair();
+			sbstp.max = Math.max(lbstp.max, Math.max(rbstp.max, root.val));
+			sbstp.min = Math.min(lbstp.min, Math.min(rbstp.min, root.val));
+			boolean isbst = lbstp.bst && rbstp.bst && lbstp.max < root.val && rbstp.min > root.val;
+			sbstp.bst = isbst;
+			return sbstp;
+		}
+
+		class BstPair {
+			boolean bst = true;
+			long min = Long.MAX_VALUE;
+			long max = Long.MIN_VALUE;
+
+		}
+	}
 }
